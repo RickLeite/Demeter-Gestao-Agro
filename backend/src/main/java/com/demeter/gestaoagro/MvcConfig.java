be954ee11ce13b08fea74d53372eb5e6cabbd66c
manoel neto <manoel.albino@proton.me>
2023-11-14 12:44:16 -0300

    fixed thymeleaf errors & merge

---
+++ backend/src/main/java/com/demeter/gestaoagro/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.demeter.gestaoagro;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+    }
+}
