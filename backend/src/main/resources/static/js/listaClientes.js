206bd9473a26ab56abe32496f627164bc3cc1400
manuela-tagliatti <manuelademoraestagliatti@yahoo.com>
2023-11-26 12:53:47 -0300

    Função gerar pdf e excel da pagina lista cliente

--- backend/src/main/resources/static/js/listaClientes.js
+++ backend/src/main/resources/static/js/listaClientes.js
@@ -82,4 +82,69 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     buscarClientes(); // Chama a função para buscar e exibir os clientes
+
+    btnGerarPDF.addEventListener('click', function() {
+        const doc = new jsPDF();
+    
+        // Criando um array com os cabeçalhos das colunas exceto 'Ação'
+        let headers = Array.from(document.querySelectorAll("#tabelaClientes thead tr th"))
+                           .map(th => th.textContent)
+                           .slice(0, -1); // Exclui o último cabeçalho 'Ação'
+    
+        // Criando um array com os dados das linhas
+        let data = Array.from(document.querySelectorAll("#tabelaClientes tbody tr")).map(tr => {
+            return Array.from(tr.querySelectorAll("td"))
+                        .slice(0, -1) // Exclui a última coluna (Ação) de cada linha
+                        .map(td => td.textContent);
+        });
+    
+        // Usando autoTable para gerar o PDF
+        doc.autoTable({
+            head: [headers],
+            body: data
+        });
+    
+        doc.save('lista-clientes.pdf');
+    });
+    
+
+    btnGerarExcel.addEventListener('click', function() {
+        let csvContent = "data:text/csv;charset=utf-8,";
+        const rows = document.querySelectorAll("#tabelaClientes tr");
+        
+        for (const row of rows) {
+            let rowData = Array.from(row.querySelectorAll("th, td"), (cell, index) => {
+                let text = cell.textContent.replace(/,/g, ";");
+    
+                // Verifica se é uma célula de dados e se é a coluna do CNPJ
+                if (cell.tagName === 'TD' && index === 5) {
+                    // Formata o CNPJ com aspas para forçar o Excel a interpretar como texto
+                    text = `="${text}"`;
+                }
+    
+                return text;
+            });
+    
+            // Remove a última célula se for uma linha de dados (coluna Ação)
+            // E também não inclui o cabeçalho da última coluna
+            if (row.querySelector("td")) {
+                rowData = rowData.slice(0, -1); // Remove a última célula (coluna Ação) das linhas de dados
+            } else {
+                // Se for a linha do cabeçalho, remove a coluna "Ação"
+                rowData = rowData.slice(0, -1); // Remove o último cabeçalho "Ação"
+            }
+    
+            csvContent += rowData.join(";") + "\r\n";
+        }
+    
+        const encodedUri = encodeURI(csvContent);
+        const link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "lista-clientes.csv");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+    
+
 });
